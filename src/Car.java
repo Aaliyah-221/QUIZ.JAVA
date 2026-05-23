15f49b31e08f4e06fa84a97a92294056d227cd7b
Aaliyah-221 <aliaumutoniwase6@gmail.com>
2026-05-23 06:50:45 +0200

    first commit

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,2 @@
-public class Car extends Vehicle{
-
-    Car(String brand, double rentalPrice) {
-        super(brand, rentalPrice);
-    }
-
-    @Override
-    double calculateRentalCost(int days) {
-        return rentalPrice * days;
-
-    }
+public class rrr {
 }
